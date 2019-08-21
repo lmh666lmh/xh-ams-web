3a402a6415d322675896e0128ff2a81fa80c6d0c
proudbin <tine>
2019-08-21 10:54:19 +0800

    付费管理-去除学校列表的充值设置

--- src/views/school/index.vue
+++ src/views/school/index.vue
@@ -99,9 +99,6 @@
                 <el-dropdown-item :command="dropdownCallback(scope.row.schoolId,'书籍管理')">
                   <el-button type="text" size="small">书籍管理</el-button>
                 </el-dropdown-item>
-                <el-dropdown-item :command="dropdownCallback(scope.row.schoolId,'充值设置')">
-                  <el-button type="text" size="small">充值设置</el-button>
-                </el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
           </template>
@@ -109,100 +106,6 @@
       </el-table>
     </div>
     <div v-show="total != 0"><Pagination :total="total" :page.sync="formInline.pageNum" :limit.sync="formInline.pageSize" @pagination="fetchData"/></div>
-    <el-dialog :visible.sync="rechargeSetForm.rechargeDialogVisible" :width="rechargeSetForm.formWidth" :close-on-click-modal="false" custom-class="addStudentDialog" title="充值设置">
-      <div style="height: 405px;overflow: auto;">
-        <el-form ref="rechargeSetForm" :model="rechargeSetForm.form" size="small">
-          <el-form-item :label-width="rechargeSetForm.formLabelWidth" label="学校信息">
-            <el-card class="box-card">
-              <p>当前学校：{{ rechargeSetForm.schoolName }}</p>
-            </el-card>
-          </el-form-item>
-          <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="自主付费" name="self">
-              <div class="tableList">
-                <el-table
-                  v-loading="selfDataListLoading"
-                  :data="selfDataList"
-                  :cell-style="cellDialogStyle"
-                  element-loading-text="Loading"
-                  border
-                  fit
-                  highlight-current-row>
-                  <el-table-column prop="projectName" label="套餐" align="center"/>
-                  <el-table-column label="基础价(￥)" align="center">
-                    <template slot-scope="scope">
-                      <span>{{ scope.row.projectPrice }}</span>
-                    </template>
-                  </el-table-column>
-                  <el-table-column label="折扣价(￥)" align="center">
-                    <template slot-scope="scope">
-                      <span v-if="scope.row.discountRatio">{{ scope.row.projectPrice * scope.row.discountRatio }}</span>
-                      <span v-else>{{ scope.row.projectPrice }}</span>
-                    </template>
-                  </el-table-column>
-                  <el-table-column label="代理分成比例" align="center">
-                    <template slot-scope="scope">
-                      <span>{{ scope.row.agentDivideRatio }}%</span>
-                    </template>
-                  </el-table-column>
-                  <el-table-column label="代理加价(￥)" align="center">
-                    <template slot-scope="scope">
-                      <el-input-number v-model="scope.row.agentAddPrice" :step="1" :precision="0" :min="0" :max="scope.row.agentAddMaxPrice" controls-position="right" size="mini" label="代理加价" @change="handleChange" />
-                    </template>
-                  </el-table-column>
-                  <el-table-column label="家长实付(￥)" align="center">
-                    <template slot-scope="scope">
-                      <span v-if="scope.row.discountRatio && scope.row.agentAddPrice">{{ (scope.row.projectPrice * scope.row.discountRatio) + scope.row.agentAddPrice }}</span>
-                      <span v-else-if="scope.row.discountRatio && !scope.row.agentAddPrice">{{ scope.row.projectPrice * scope.row.discountRatio }}</span>
-                      <span v-else-if="!scope.row.discountRatio && scope.row.agentAddPrice">{{ scope.row.projectPrice + scope.row.agentAddPrice }}</span>
-                      <span v-else>{{ scope.row.projectPrice }}</span>
-                    </template>
-                  </el-table-column>
-                  <el-table-column :render-header="renderHeader" label="是否显示" width="150" align="center">
-                    <template slot-scope="scope">
-                      <el-switch
-                        v-model="scope.row.display"
-                        active-color="#13ce66"
-                        inactive-color="#DBDFE6"/>
-                    </template>
-                  </el-table-column>
-                </el-table>
-              </div>
-              <div style="margin-top: 20px;color: #a5a6a8;font-size: 12px;">
-                <p>注：1、折扣价为小禾官方推出的不定期优惠活动价</p>
-                <P style="text-indent: 1.5rem;">2、家长实付 = 折扣价 + 代理加价 （不包含押金）</P>
-                <p style="text-indent: 1.5rem;">3、代理商收益 = 折扣价 * 分成比例 + 代理加价</p>
-                <p style="text-indent: 1.5rem;">4、代理加价存在上下限，如需调整，请联系：<span style="color: blue;">18046218009</span></p>
-                <p style="text-indent: 1.5rem;">5、【是否显示】表示在小程序会员中是否显示该会员套餐</p>
-              </div>
-            </el-tab-pane>
-            <el-tab-pane label="全园付费" name="all">
-              <div class="tableList">
-                <el-table
-                  v-loading="allDataListLoading"
-                  :data="allDataList"
-                  :cell-style="cellDialogStyle"
-                  element-loading-text="Loading"
-                  border
-                  fit
-                  highlight-current-row>
-                  <el-table-column prop="projectName" label="套餐" align="center"/>
-                  <el-table-column label="价格" align="center">
-                    <template slot-scope="scope">
-                      <span>{{ scope.row.projectPrice | toRMB }}</span>
-                    </template>
-                  </el-table-column>
-                </el-table>
-              </div>
-            </el-tab-pane>
-          </el-tabs>
-        </el-form>
-      </div>
-      <div slot="footer" class="dialog-footer">
-        <el-button size="small" @click="cancel">取 消</el-button>
-        <el-button size="small" type="primary" @click="confirm">确 定</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
@@ -237,18 +140,7 @@ export default {
       cityOptions: [],
       countryOptions: [],
       ajaxLoading: null,
-      uploadUrl: SYSTEM.SCHOOL.IMPORTSCHOOL,
-      rechargeSetForm: {
-        rechargeDialogVisible: false,
-        formWidth: '900px',
-        formLabelWidth: '70px',
-        schoolName: '',
-        schoolId: ''
-      },
-      selfDataList: [],
-      selfDataListLoading: false,
-      allDataListLoading: false,
-      allDataList: []
+      uploadUrl: SYSTEM.SCHOOL.IMPORTSCHOOL
     }
   },
   computed: {
@@ -276,12 +168,6 @@ export default {
     }
   },
   methods: {
-    toRMB: value => {
-      return `${(value / 100).toFixed(2)}` - 0
-    },
-    toCents: value => {
-      return `${value * 100}`
-    },
     onSubmit() {
       this.formInline.pageNum = 1
       this.fetchData()
@@ -399,94 +285,6 @@ export default {
     down(url) {
       api.download(url)
     },
-    rechargeSet(schoolId) {
-      this.rechargeSetForm.rechargeDialogVisible = true
-      this.rechargeSetForm.schoolId = schoolId
-      api.getDetailRecharge({
-        schoolId: schoolId
-      }).then(response => {
-        if (response.code === 10000) {
-          this.rechargeSetForm.schoolName = response.data.schoolName
-          response.data.autoPay.forEach((item, index) => {
-            for (const key in item) {
-              if (key.match('Price') || key === 'discountRatio') {
-                item[key] = this.toRMB(item[key])
-              }
-              if (key.match('display')) {
-                item[key] = !!item[key]
-              }
-            }
-          })
-          this.selfDataList = response.data.autoPay
-          this.allDataList = response.data.allPay
-        }
-      }).catch(() => {
-
-      })
-    },
-    confirm() {
-      const loading = this.$loading({
-        lock: true,
-        text: 'Loading',
-        spinner: 'el-icon-loading',
-        background: 'rgba(0, 0, 0, 0.7)'
-      })
-      const updateList = []
-      this.selfDataList.forEach((item, index) => {
-        updateList.push({
-          projectId: item.projectId,
-          projectPrice: this.toCents(item.projectPrice) - 0,
-          agentAddPrice: this.toCents(item.agentAddPrice) - 0,
-          display: item.display ? 1 : 0
-        })
-      })
-      api.updateRechargeSet({ ...{ updateList: updateList }, ...{ schoolId: this.rechargeSetForm.schoolId }}).then(response => {
-        loading.close()
-        if (response.code === 10000) {
-          this.rechargeSetForm.rechargeDialogVisible = false
-          this.$message({
-            message: '设置成功',
-            type: 'success'
-          })
-          this.fetchData()
-        } else {
-          this.$message.error('设置失败')
-        }
-      }).catch(() => {
-        loading.close()
-      })
-    },
-    cancel() {
-      this.rechargeSetForm.rechargeDialogVisible = false
-    },
-    handleClick(tab, event) {
-      console.log(tab, event)
-    },
-    // render 事件
-    renderHeader(h, { column }) {
-      return [
-        column.label,
-        h(
-          'el-tooltip',
-          {
-            props: {
-              content: '是否在小程序中显示',
-              placement: 'top'
-            }
-          },
-          [
-            h('span', {
-              class: {
-                'el-icon-question': true
-              }
-            })
-          ]
-        )
-      ]
-    },
-    handleChange(value) {
-      console.log(value)
-    },
     dropdownCallback(schoolId, type) {
       return {
         schoolId: schoolId,
@@ -504,8 +302,6 @@ export default {
         this.routeTo('/school/bookcaseDetail', command.schoolId)
       } else if (command.type === '书籍管理') {
         this.routeTo('/books/index', command.schoolId)
-      } else if (command.type === '充值设置') {
-        this.rechargeSet(command.schoolId)
       }
     }
   }
